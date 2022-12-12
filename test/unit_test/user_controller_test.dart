1fd076b43e8b5557edcde66a87028f9ce2666e4a
bryo <bryoastyl@gmail.com>
2022-12-12 10:03:53 +0300

    Unit tests for Farm and user controllers  - units tests

---
+++ test/unit_test/user_controller_test.dart
@@ -0,0 +1,44 @@
+import 'package:epoultry/controllers/user_controller.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('UserController', () {
+    late UserController controller;
+    setUp(() {
+      controller = UserController();
+    });
+
+    test('updateName', () {
+      // Given
+      var name = 'Me Two';
+
+      // When
+      controller.updateName(name);
+
+      // Then
+      expect(controller.userName.value, name);
+    });
+
+    test('updatePhone', () {
+      // Given
+      var phone = '0711800260';
+
+      // When
+      controller.updatePhone(phone);
+
+      // Then
+      expect(controller.phoneNumber.value, phone);
+    });
+
+    test('updateRole', () {
+      // Given
+      var role = 'admin';
+
+      // When
+      controller.updateRole(role);
+
+      // Then
+      expect(controller.userRole.value, role);
+    });
+  });
+}
