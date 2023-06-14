7b8848087b9d6adaa4de7a383a4afe870b7a6caf
KenStarry <starrycodes@gmail.com>
2023-06-14 10:34:51 +0300

    Refactored snackbar to enable code re-use

--- lib/core/utils/reactive_snackbar.dart
+++ lib/core/utils/reactive_snackbar.dart
@@ -1,3 +1,4 @@
+import 'package:epoultry/core/presentation/components/custom_snackbar.dart';
 import 'package:epoultry/core/presentation/controllers/user_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,62 +9,22 @@ import '../../theme/colors.dart';
 void reactiveSnackbar({required UserController userController}) =>
     ever(userController.hasInternetObservable, (value) {
       if (value == true) {
-        return Get.showSnackbar(GetSnackBar(
-          titleText: Text(
-            "Connected",
-            style: TextStyle(
-                color: CustomColors.green,
-                fontFamily:
-                    Theme.of(Get.context!).textTheme.titleMedium?.fontFamily,
-                fontSize:
-                    Theme.of(Get.context!).textTheme.titleMedium?.fontSize),
-          ),
-          messageText: Text(
-            "Internet connection established",
-            style: TextStyle(
-                color: CustomColors.white,
-                fontFamily:
-                Theme.of(Get.context!).textTheme.bodyMedium?.fontFamily,
-                fontSize:
-                Theme.of(Get.context!).textTheme.bodyMedium?.fontSize),
-          ),
-          backgroundColor: CustomColors.snackbarBackground,
-          icon: const Icon(
-            Icons.wifi,
-            color: CustomColors.green,
-          ),
-          dismissDirection: DismissDirection.down,
-          duration: const Duration(seconds: 3),
-          margin: const EdgeInsets.all(16),
-          borderRadius: 16,
-        ));
+        return Get.showSnackbar(customSnackbar(
+            title: "Connected",
+            message: "Internet connection established",
+            titleColor: CustomColors.green,
+            messageColor: CustomColors.white,
+            icon: Icons.wifi,
+            iconColor: CustomColors.green,
+            backgroundColor: CustomColors.snackbarBackground));
       } else {
-        return Get.showSnackbar(GetSnackBar(
-          titleText: Text("Disconnected",
-              style: TextStyle(
-                  color: CustomColors.redLight,
-                  fontFamily:
-                      Theme.of(Get.context!).textTheme.titleMedium?.fontFamily,
-                  fontSize:
-                      Theme.of(Get.context!).textTheme.titleMedium?.fontSize)),
-          messageText: Text(
-            "Internet connection lost",
-            style: TextStyle(
-                color: CustomColors.white,
-                fontFamily:
-                    Theme.of(Get.context!).textTheme.bodyMedium?.fontFamily,
-                fontSize:
-                    Theme.of(Get.context!).textTheme.bodyMedium?.fontSize),
-          ),
-          backgroundColor: CustomColors.snackbarBackground,
-          icon: const Icon(
-            Icons.wifi_off,
-            color: CustomColors.redLight,
-          ),
-          dismissDirection: DismissDirection.down,
-          duration: const Duration(seconds: 3),
-          margin: const EdgeInsets.all(16),
-          borderRadius: 16,
-        ));
+        return Get.showSnackbar(customSnackbar(
+            title: "Disconnected",
+            message: "Internet connection lost",
+            titleColor: CustomColors.redLight,
+            messageColor: CustomColors.white,
+            icon: Icons.wifi_off,
+            iconColor: CustomColors.redLight,
+            backgroundColor: CustomColors.snackbarBackground));
       }
     });
